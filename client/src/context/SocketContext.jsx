1a62aa2340344861fe4d012cd5248b960d2fbb7f
IncharaS <60838958+IncharaS@users.noreply.github.com>
2025-03-20 00:03:19 -0400

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-application-vp94.onrender.com", {
+			const socket = io("https://chat-application-vp94.onrender.com:8080", {
 				query: {
 					userId: authUser._id,
 				},
